ff24e2d55f4f858c6561f2d0f4a36851c04092db
gulindo0805 <159536539+gulindo0805@users.noreply.github.com>
2024-02-23 11:55:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,4 +32,19 @@
             },
         });
     </script>
+<script>
+    function changeColors() {
+      document.body.style.backgroundColor = getRandomColor();
+      document.body.style.color = getRandomColor();
+    }
+
+    function getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+  </script>
 
