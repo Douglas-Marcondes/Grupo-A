b7e610002c48af41e792d5e8ce366a1aa7b0d185
Douglas-Marcondes <159036433+Douglas-Marcondes@users.noreply.github.com>
2024-03-15 07:38:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
-let inputBox = document.querySelector(".input-box"),
-                searchIcon = document.querySelector(".icon"),
-                closeIcon = document.querySelector(".close-icon");
-
-            searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
-            closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
 
             document.getElementById('item-flutuante').addEventListener('mouseover', function() {
                 document.getElementById('janela-flutuante').classList.add('show');
