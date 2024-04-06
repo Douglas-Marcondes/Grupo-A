7b2948e9693395414b74af01e481d95820f13c12
davidejesus123 <161027753+davidejesus123@users.noreply.github.com>
2024-04-05 21:17:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 1000); // Change image every 10 seconds
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
